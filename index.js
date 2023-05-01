c42efb6b0e4a17dcf6d448c5f2f6db884f945694
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-05-02 01:06:10 +0600

    create chef data

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express=require('express');
 const app=express();
 const port=process.env.PORT || 1020;
 const cors=require('cors');
+const chef=require('./chef.json');
 
 app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send("Assignment 10 Server Ok")
+})
+
+app.get('/chef',(req,res)=>{
+    res.send(chef)
 })
 
 app.listen(port,()=>{
